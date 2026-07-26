f7673dea1e7a643e7c961f62923d0b3bb97bc692
Benedict <benedictpatrickjohn@gmail.com>
2026-07-27 01:51:25 +0530

    Increase haptic vibration duration

--- src/utils/haptics.js
+++ src/utils/haptics.js
@@ -1,6 +1,6 @@
 export const hapticLight = () => {
   try {
-    if (navigator && navigator.vibrate) navigator.vibrate(10);
+    if (navigator && navigator.vibrate) navigator.vibrate(30);
   } catch (e) {
     // Ignore errors on devices that don't support vibration
   }
@@ -8,7 +8,7 @@ export const hapticLight = () => {
 
 export const hapticMedium = () => {
   try {
-    if (navigator && navigator.vibrate) navigator.vibrate(20);
+    if (navigator && navigator.vibrate) navigator.vibrate(50);
   } catch (e) {
     // Ignore
   }
@@ -16,7 +16,7 @@ export const hapticMedium = () => {
 
 export const hapticHeavy = () => {
   try {
-    if (navigator && navigator.vibrate) navigator.vibrate(40);
+    if (navigator && navigator.vibrate) navigator.vibrate(80);
   } catch (e) {
     // Ignore
   }
@@ -24,7 +24,7 @@ export const hapticHeavy = () => {
 
 export const hapticSuccess = () => {
   try {
-    if (navigator && navigator.vibrate) navigator.vibrate([10, 30, 20]);
+    if (navigator && navigator.vibrate) navigator.vibrate([30, 50, 40]);
   } catch (e) {
     // Ignore
   }
@@ -32,7 +32,7 @@ export const hapticSuccess = () => {
 
 export const hapticError = () => {
   try {
-    if (navigator && navigator.vibrate) navigator.vibrate([30, 50, 30, 50, 40]);
+    if (navigator && navigator.vibrate) navigator.vibrate([50, 50, 50, 50, 80]);
   } catch (e) {
     // Ignore
   }
